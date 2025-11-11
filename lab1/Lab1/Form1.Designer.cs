5993696131075d6d23348c46b23190cf97477f0e
Dmitry Eleckiy <deletskii@sfedu.ru>
2025-11-11 21:26:10 +0300

    Lab1

---
+++ lab1/Lab1/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.graphPictureBox = new System.Windows.Forms.PictureBox();
+            this.functionComboBox = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.graphPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // graphPictureBox
+            // 
+            this.graphPictureBox.BackColor = System.Drawing.SystemColors.MenuText;
+            this.graphPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.graphPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.graphPictureBox.Margin = new System.Windows.Forms.Padding(30, 3, 3, 3);
+            this.graphPictureBox.Name = "graphPictureBox";
+            this.graphPictureBox.Size = new System.Drawing.Size(800, 450);
+            this.graphPictureBox.TabIndex = 0;
+            this.graphPictureBox.TabStop = false;
+            this.graphPictureBox.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.graphPictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.graphPictureBox_Paint);
+            // 
+            // functionComboBox
+            // 
+            this.functionComboBox.FormattingEnabled = true;
+            this.functionComboBox.Location = new System.Drawing.Point(12, 12);
+            this.functionComboBox.Name = "functionComboBox";
+            this.functionComboBox.Size = new System.Drawing.Size(65, 21);
+            this.functionComboBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.functionComboBox);
+            this.Controls.Add(this.graphPictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.graphPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox graphPictureBox;
+        private System.Windows.Forms.ComboBox functionComboBox;
+    }
+}
+
---
+++ lab1/Lab1/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+public delegate double FunctionDelegate(double x);
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, FunctionDelegate> availableFunctions;
+
+        public double SinFunction(double x) => Math.Sin(x);
+        public double XSquaredFunction(double x) => x * x;
+        public double CosFunction(double x) => Math.Cos(x);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            availableFunctions = new Dictionary<string, FunctionDelegate>
+            {
+                {"sin(x)", SinFunction},
+                {"x^2", XSquaredFunction},
+                {"cos(x)", CosFunction}
+            };
+
+            functionComboBox.Items.AddRange(availableFunctions.Keys.ToArray());
+            functionComboBox.SelectedIndex = 0;
+            //усли форма изменяет размер или выбирается другая функция то вызывается graphPictureBox.Invalidate();
+            this.Resize += (s, ev) => graphPictureBox.Invalidate();
+            functionComboBox.SelectedIndexChanged += (s, ev) => graphPictureBox.Invalidate();
+        }
+
+        private void PlotGraph(Graphics g, int width, int height, FunctionDelegate function)
+        {
+            double xMin = -10.0;
+            double xMax = 10.0;
+            double step = (xMax - xMin) / width;
+
+            List<double> yValues = new List<double>();
+
+            for (int i = 0; i < width; i++)
+            {
+                double x = xMin + i * step;
+                double y = function(x);
+                yValues.Add(y);
+            }
+
+            double yMin = yValues.Min();
+            double yMax = yValues.Max();
+
+            if (Math.Abs(yMax - yMin) < 0.0001)
+            {
+                yMax += 1.0;
+                yMin -= 1.0;
+            }
+
+            //оси
+            Pen axisPen = new Pen(Color.White, 2);
+
+            if (yMin <= 0 && 0 <= yMax)
+            {
+                float normalizedYZero = (float)((0.0 - yMin) / (yMax - yMin));
+                float screenYZero = height - (normalizedYZero * height);
+                g.DrawLine(axisPen, 0, screenYZero, width, screenYZero);
+            }
+
+            if (xMin <= 0 && 0 <= xMax)
+            {
+                float normalizedXZero = (float)((0.0 - xMin) / (xMax - xMin));
+                float screenXZero = normalizedXZero * width;
+                g.DrawLine(axisPen, screenXZero, 0, screenXZero, height);
+            }
+
+            //график
+            Pen graphPen = new Pen(Color.Red, 4);
+            PointF previousPoint = PointF.Empty; //предыдущая точка
+
+            for (int i = 0; i < width; i++)
+            {
+                double yValue = yValues[i];
+
+                float screenX = i;
+                float normalizedY = (float)((yValue - yMin) / (yMax - yMin));
+                float screenY = height - (normalizedY * height);
+
+                PointF currentPoint = new PointF(screenX, screenY);
+
+                if (i > 0)
+                {
+                    g.DrawLine(graphPen, previousPoint, currentPoint);
+                }
+                previousPoint = currentPoint;
+            }
+        }
+
+        private void graphPictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            int width = graphPictureBox.Width;
+            int height = graphPictureBox.Height;
+
+            string selectedKey = functionComboBox.SelectedItem.ToString();
+            FunctionDelegate currentFunction = availableFunctions[selectedKey];
+
+            PlotGraph(g, width, height, currentFunction);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
