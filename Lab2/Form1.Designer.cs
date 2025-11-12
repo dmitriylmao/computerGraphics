08bf5d50c292de481c1deaf80ccea83bf6456d63
Dmitry Eleckiy <deletskii@sfedu.ru>
2025-11-12 13:37:10 +0300

    Lab2

---
+++ Lab2/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadButton = new System.Windows.Forms.Button();
+            this.sourcePictureBox = new System.Windows.Forms.PictureBox();
+            this.resultPictureBox = new System.Windows.Forms.PictureBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.hueLabel = new System.Windows.Forms.Label();
+            this.hueTrackBar = new System.Windows.Forms.TrackBar();
+            this.satTrackBar = new System.Windows.Forms.TrackBar();
+            this.valTrackBar = new System.Windows.Forms.TrackBar();
+            this.satLabel = new System.Windows.Forms.Label();
+            this.valLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.sourcePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resultPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hueTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.satTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.valTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(16, 26);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(75, 23);
+            this.loadButton.TabIndex = 0;
+            this.loadButton.Text = "открыть";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // sourcePictureBox
+            // 
+            this.sourcePictureBox.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.sourcePictureBox.Location = new System.Drawing.Point(231, 12);
+            this.sourcePictureBox.Name = "sourcePictureBox";
+            this.sourcePictureBox.Size = new System.Drawing.Size(585, 602);
+            this.sourcePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.sourcePictureBox.TabIndex = 1;
+            this.sourcePictureBox.TabStop = false;
+            // 
+            // resultPictureBox
+            // 
+            this.resultPictureBox.BackColor = System.Drawing.SystemColors.Control;
+            this.resultPictureBox.Location = new System.Drawing.Point(839, 12);
+            this.resultPictureBox.Name = "resultPictureBox";
+            this.resultPictureBox.Size = new System.Drawing.Size(585, 602);
+            this.resultPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.resultPictureBox.TabIndex = 2;
+            this.resultPictureBox.TabStop = false;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(16, 75);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 3;
+            this.saveButton.Text = "сохранить";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // hueLabel
+            // 
+            this.hueLabel.AutoSize = true;
+            this.hueLabel.Location = new System.Drawing.Point(51, 174);
+            this.hueLabel.Name = "hueLabel";
+            this.hueLabel.Size = new System.Drawing.Size(35, 13);
+            this.hueLabel.TabIndex = 4;
+            this.hueLabel.Text = "label1";
+            // 
+            // hueTrackBar
+            // 
+            this.hueTrackBar.Location = new System.Drawing.Point(16, 190);
+            this.hueTrackBar.Maximum = 180;
+            this.hueTrackBar.Minimum = -180;
+            this.hueTrackBar.Name = "hueTrackBar";
+            this.hueTrackBar.Size = new System.Drawing.Size(104, 45);
+            this.hueTrackBar.TabIndex = 5;
+            this.hueTrackBar.TickFrequency = 30;
+            // 
+            // satTrackBar
+            // 
+            this.satTrackBar.Location = new System.Drawing.Point(16, 287);
+            this.satTrackBar.Maximum = 100;
+            this.satTrackBar.Minimum = -100;
+            this.satTrackBar.Name = "satTrackBar";
+            this.satTrackBar.Size = new System.Drawing.Size(104, 45);
+            this.satTrackBar.TabIndex = 6;
+            this.satTrackBar.TickFrequency = 10;
+            // 
+            // valTrackBar
+            // 
+            this.valTrackBar.Location = new System.Drawing.Point(16, 373);
+            this.valTrackBar.Maximum = 100;
+            this.valTrackBar.Minimum = -100;
+            this.valTrackBar.Name = "valTrackBar";
+            this.valTrackBar.Size = new System.Drawing.Size(104, 45);
+            this.valTrackBar.TabIndex = 7;
+            this.valTrackBar.TickFrequency = 10;
+            // 
+            // satLabel
+            // 
+            this.satLabel.AutoSize = true;
+            this.satLabel.Location = new System.Drawing.Point(51, 271);
+            this.satLabel.Name = "satLabel";
+            this.satLabel.Size = new System.Drawing.Size(35, 13);
+            this.satLabel.TabIndex = 8;
+            this.satLabel.Text = "label1";
+            // 
+            // valLabel
+            // 
+            this.valLabel.AutoSize = true;
+            this.valLabel.Location = new System.Drawing.Point(51, 357);
+            this.valLabel.Name = "valLabel";
+            this.valLabel.Size = new System.Drawing.Size(35, 13);
+            this.valLabel.TabIndex = 9;
+            this.valLabel.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1434, 626);
+            this.Controls.Add(this.valLabel);
+            this.Controls.Add(this.satLabel);
+            this.Controls.Add(this.valTrackBar);
+            this.Controls.Add(this.satTrackBar);
+            this.Controls.Add(this.hueTrackBar);
+            this.Controls.Add(this.hueLabel);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.resultPictureBox);
+            this.Controls.Add(this.sourcePictureBox);
+            this.Controls.Add(this.loadButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.sourcePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resultPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hueTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.satTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.valTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.PictureBox sourcePictureBox;
+        private System.Windows.Forms.PictureBox resultPictureBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label hueLabel;
+        private System.Windows.Forms.TrackBar hueTrackBar;
+        private System.Windows.Forms.TrackBar satTrackBar;
+        private System.Windows.Forms.TrackBar valTrackBar;
+        private System.Windows.Forms.Label satLabel;
+        private System.Windows.Forms.Label valLabel;
+    }
+}
+
---
+++ Lab2/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap originalImage;
+        private Bitmap processedImage;
+
+        private int hueShift = 0;
+        private int satShift = 0;
+        private int valShift = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            hueTrackBar.Scroll += HsvTrackBar_Scroll;
+            satTrackBar.Scroll += HsvTrackBar_Scroll;
+            valTrackBar.Scroll += HsvTrackBar_Scroll;
+
+            UpdateLabels();
+            saveButton.Enabled = false;
+        }
+
+        private void UpdateLabels()
+        {
+            hueLabel.Text = $"H (Оттенок): {hueTrackBar.Value}°";
+            satLabel.Text = $"S (Насыщенность): {satTrackBar.Value}%";
+            valLabel.Text = $"V (Яркость): {valTrackBar.Value}%";
+        }
+
+        private void HsvTrackBar_Scroll(object sender, EventArgs e)
+        {
+            hueShift = hueTrackBar.Value;
+            satShift = satTrackBar.Value;
+            valShift = valTrackBar.Value;
+
+            UpdateLabels();
+
+            if (originalImage != null)
+            {
+                ProcessImageFast();
+            }
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Image Files|*.bmp;*.jpg;*.jpeg;*.png;*.gif";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    originalImage = new Bitmap(ofd.FileName);
+                    sourcePictureBox.Image = originalImage;
+
+                    ProcessImageFast();
+
+                    saveButton.Enabled = true;
+                }
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (processedImage == null) return;
+
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.Filter = "PNG Image|*.png|JPEG Image|*.jpg";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    processedImage.Save(sfd.FileName);
+                }
+            }
+        }
+
+        private void ProcessImageFast()
+        {
+            if (originalImage == null) return;
+
+            int width = originalImage.Width;
+            int height = originalImage.Height;
+
+            processedImage = (Bitmap)originalImage.Clone();
+
+            Rectangle rect = new Rectangle(0, 0, width, height);
+            //получаем доступ к пикселям
+            BitmapData bmpData = processedImage.LockBits(rect, ImageLockMode.ReadWrite, processedImage.PixelFormat);
+
+            IntPtr ptr = bmpData.Scan0;
+
+            int bytes = Math.Abs(bmpData.Stride) * height;
+            byte[] rgbValues = new byte[bytes];
+
+            Marshal.Copy(ptr, rgbValues, 0, bytes);
+
+            int pixelSize = Image.GetPixelFormatSize(processedImage.PixelFormat) / 8;
+
+            for (int i = 0; i < bytes; i += pixelSize)
+            {
+                int b = rgbValues[i];
+                int g = rgbValues[i + 1];
+                int r = rgbValues[i + 2];
+
+                RgbToHsv(r, g, b, out double h, out double s, out double v);
+
+                double hPrime = h + hueShift;
+                double sPrime = s + (double)satShift / 100.0;
+                double vPrime = v + (double)valShift / 100.0;
+
+                if (hPrime > 360) hPrime -= 360;
+                if (hPrime < 0) hPrime += 360;
+                sPrime = Math.Max(0.0, Math.Min(1.0, sPrime));
+                vPrime = Math.Max(0.0, Math.Min(1.0, vPrime));
+
+                Color newColor = HsvToRgb(hPrime, sPrime, vPrime);
+
+                rgbValues[i] = newColor.B;
+                rgbValues[i + 1] = newColor.G;
+                rgbValues[i + 2] = newColor.R;
+            }
+
+            Marshal.Copy(rgbValues, 0, ptr, bytes);
+
+            processedImage.UnlockBits(bmpData);
+
+            resultPictureBox.Image = processedImage;
+        }
+
+        private void RgbToHsv(int r, int g, int b, out double h, out double s, out double v)
+        {
+            double rNorm = r / 255.0;
+            double gNorm = g / 255.0;
+            double bNorm = b / 255.0;
+
+            double max = Math.Max(rNorm, Math.Max(gNorm, bNorm));
+            double min = Math.Min(rNorm, Math.Min(gNorm, bNorm));
+            double delta = max - min;
+
+            v = max;
+
+            if (max == 0.0)
+            {
+                s = 0;
+                h = 0;
+                return;
+            }
+
+            s = delta / max;
+
+            if (delta == 0.0)
+            {
+                h = 0;
+                return;
+            }
+
+            if (max == rNorm)
+            {
+                h = 60.0 * ((gNorm - bNorm) / delta);
+            }
+            else if (max == gNorm)
+            {
+                h = 60.0 * ((bNorm - rNorm) / delta + 2.0);
+            }
+            else
+            {
+                h = 60.0 * ((rNorm - gNorm) / delta + 4.0);
+            }
+
+            if (h < 0.0)
+            {
+                h += 360.0;
+            }
+        }
+
+        private Color HsvToRgb(double h, double s, double v)
+        {
+            double r = 0, g = 0, b = 0;
+
+            if (s == 0)
+            {
+                r = g = b = v;
+            }
+            else
+            {
+                double sector = h / 60.0;
+                int i = (int)Math.Floor(sector);
+                double f = sector - i;
+                double p = v * (1.0 - s);
+                double q = v * (1.0 - s * f);
+                double t = v * (1.0 - s * (1.0 - f));
+
+                switch (i)
+                {
+                    case 0: r = v; g = t; b = p; break;
+                    case 1: r = q; g = v; b = p; break;
+                    case 2: r = p; g = v; b = t; break;
+                    case 3: r = p; g = q; b = v; break;
+                    case 4: r = t; g = p; b = v; break;
+                    default: r = v; g = p; b = q; break;
+                }
+            }
+
+            return Color.FromArgb(
+                (int)Math.Round(Math.Min(255.0, r * 255.0)),
+                (int)Math.Round(Math.Min(255.0, g * 255.0)),
+                (int)Math.Round(Math.Min(255.0, b * 255.0))
+            );
+        }
+    }
+}
